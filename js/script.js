7b07c77695c481da29af94fdf314154d15c764d0
Angela Polo <polo0016@algonquinlive.com>
2022-08-09 09:46:31 -0400

    Writing description on readme file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 /* Styling Window Load functions */
+
 function imgbox_height(){
     var img_height = $('.teammember-detail .detailbox .img-box').outerWidth();
     $('.teammember-detail .detailbox .img-box').css('height', img_height);
 }
+
 function main_top(){
     var heading_height = $('header').outerHeight();
     $('.main').css('padding-top',heading_height);
 }
+
 function mobile_menu(){
     var mob_menu = $('header .header-bottom .menu-area').html();
     $('#mobile_menu .menu-area').append(mob_menu);
@@ -20,16 +23,20 @@ $(window).on('load',function(){
 
 
 $(document).ready(function(){
+
     imgbox_height();
     main_top();
     mobile_menu();
+
     if($(window).width() < 768){
         $(".banner-area .btn").text("Visit our Shop");
     }
+
     $('.menu-area nav ul  li').on('click', function(){
         $('.menu-area nav ul  li').removeClass('active');
         $(this).toggleClass('active');
     });
+    
     $( '.page-menu ul a').on( 'click', function(e){
         var href = $(this).attr( 'href' );
         $( 'html, body' ).animate({ 
@@ -38,21 +45,26 @@ $(document).ready(function(){
         e.preventDefault();
         $('#mobile_menu').removeClass('open');
       });
+
       $('header .hamburger').on('click', function(e){
         e.stopPropagation();
         $('#mobile_menu').addClass('open');
     });
+
     $('header .header-top-area .search ').on('click', function(e){
         e.stopPropagation();
         $('#search_box').toggleClass('open');
     });
+
     $('#mobile_menu .close-menu').on('click', function(){
         $('#mobile_menu').removeClass('open');
     });
+
     $('.project-gallery .project-box > a').on('click', function(){
         var img_src = $(this).children('img').attr('src');
         $('.portfolio-section .portfolio-main-img-box .img-box img').attr('src',img_src);
     });
+
     setTimeout(function() {
         $('#preloader').removeClass('show');
     }, 1200);  
@@ -63,12 +75,16 @@ $(window).on('resize',function(){
     main_top();
     imgbox_height();
 });
+
 $(window).scroll(function(){
     if ($(this).scrollTop() > 210) {
        $('header').addClass('fixed');
-    } else {
+    } 
+
+    else {
        $('header').removeClass('fixed');
     }
+
     if($(window).width() < 1661){
         if ($(this).scrollTop() > 80) {
             $('header').addClass('fixed');
@@ -77,10 +93,12 @@ $(window).scroll(function(){
          }
     }
 });
+
 $(document).click(function (e) {
     if(!$(e.target).closest('#mobile_menu').length){
       $("#mobile_menu").removeClass('open');
     }
+    
     if(!$(e.target).closest('#search_box').length){
         $("#search_box").removeClass('open');
       }
